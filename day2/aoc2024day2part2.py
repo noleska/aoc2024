b61c08c937ce92501c6aa0d4eaf2ad80811bd6be
noleska <83671681+noleska@users.noreply.github.com>
2024-12-02 16:10:04 -0500

    Update aoc2024day2part2.py

--- day2/aoc2024day2part2.py
+++ day2/aoc2024day2part2.py
@@ -13,15 +13,13 @@ class report:
     def __init__(self, levels, dampen=True):
         self.dampen = dampen
         self.levels = [int(level) for level in levels]
-        self.changes = [self.levels[i+1] - self.levels[i] \
-                        for i in range(len(self.levels)-1)]
+        self.changes = [self.levels[i+1] - self.levels[i] for i in range(len(self.levels)-1)]
         self.abs_changes = [abs(x) for x in self.changes]
         self.overchanges = max(self.abs_changes) > 3 or min(self.abs_changes) < 1
         self.contains_ascend = len([x for x in self.changes if x > 0]) > 0
         self.contains_descend = len([x for x in self.changes if x < 0]) > 0
         self.switches_direction = self.contains_ascend and self.contains_descend
-        self.inherent_safe = not self.switches_direction \
-                             and not self.overchanges
+        self.inherent_safe = not self.switches_direction and not self.overchanges
 
         # problem dampener
         self.dampened_safe = False
@@ -36,12 +34,9 @@ class report:
         self.safe = self.inherent_safe or self.dampened_safe
             
 
-
 if __name__ == '__main__':
     input_loc = r'./input.txt'
-    
     reports = []
-    
     with open(input_loc, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=' ')
         for row in spamreader:
