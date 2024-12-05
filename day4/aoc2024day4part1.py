e05c0e54ef29df3ebd72e17764ea6707678d6499
noleska <83671681+noleska@users.noreply.github.com>
2024-12-04 23:52:06 -0500

    Update aoc2024day4part1.py
    easy optimize and minor clean-up

--- day4/aoc2024day4part1.py
+++ day4/aoc2024day4part1.py
@@ -20,11 +20,11 @@ pts_for_dir = {'n' : [],
                'nw': []}
 
 for i in range(len(search_key)):
-  pts_for_dir['n' ].append({'x': -i , 'y' : 0})
-  pts_for_dir['ne'].append({'x' : -i, 'y' : i})
-  pts_for_dir['e' ].append({'x' : 0 , 'y' : i})
-  pts_for_dir['se'].append({'x' : i , 'y' : i})
-  pts_for_dir['s' ].append({'x' : i , 'y' : 0})
+  pts_for_dir['n' ].append({'x' : -i, 'y' : 0 })
+  pts_for_dir['ne'].append({'x' : -i, 'y' : i })
+  pts_for_dir['e' ].append({'x' : 0 , 'y' : i })
+  pts_for_dir['se'].append({'x' : i , 'y' : i })
+  pts_for_dir['s' ].append({'x' : i , 'y' : 0 })
   pts_for_dir['sw'].append({'x' : i , 'y' : -i})
   pts_for_dir['w' ].append({'x' : 0 , 'y' : -i})
   pts_for_dir['nw'].append({'x' : -i, 'y' : -i})
@@ -35,12 +35,12 @@ class search_point:
     self.xorigin = x
     self.yorigin = y
     self.clear = {'n' : self.xorigin >= nbound + margin,
-                  'e' : self.yorigin < ebound - margin,
-                  's' : self.xorigin < sbound - margin,
+                  'e' : self.yorigin <  ebound - margin,
+                  's' : self.xorigin <  sbound - margin,
                   'w' : self.yorigin >= wbound + margin,
-                  'ne': self.xorigin >= nbound + margin and self.yorigin < ebound - margin,
-                  'se': self.xorigin < sbound - margin and self.yorigin < ebound - margin,
-                  'sw': self.xorigin < sbound - margin and self.yorigin >= wbound + margin,
+                  'ne': self.xorigin >= nbound + margin and self.yorigin <  ebound - margin,
+                  'se': self.xorigin <  sbound - margin and self.yorigin <  ebound - margin,
+                  'sw': self.xorigin <  sbound - margin and self.yorigin >= wbound + margin,
                   'nw': self.xorigin >= nbound + margin and self.yorigin >= wbound + margin}
     self.num_matches = 0
 
@@ -55,5 +55,6 @@ class search_point:
 tot_matches = 0
 for row in range(0,len(df)):
   for col in range(0,len(df.columns)):
-    tot_matches += search_point(row, col).num_matches
+    if df.iloc[row, col] == search_key[0]:
+      tot_matches += search_point(row, col).num_matches
 print("Part 1:", tot_matches)
